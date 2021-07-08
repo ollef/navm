a19a11e9a94a7c71cac8276f57e23d30dcec05a3
Olle Fredriksson <fredriksson.olle@gmail.com>
2021-07-09 00:56:02 +0200

    Add postorder graph traversal

--- src/graph.rs
+++ src/graph.rs
@@ -585,3 +585,54 @@ pub trait Fact {
     fn bottom() -> Self;
     fn join(old_fact: Self, new_fact: Self) -> Self;
 }
+
+struct Postorder<'a, Label, Initiator, Instruction, Terminator> {
+    todo: Vec<Label>,
+    visited: HashSet<Label>,
+    graph: &'a GraphOC<Label, Initiator, Instruction, Terminator>,
+}
+
+impl<'a, Label, Initiator, Instruction, Terminator> Iterator
+    for Postorder<'a, Label, Initiator, Instruction, Terminator>
+where
+    Terminator: Terminate<Label>,
+    Label: Eq + Hash + Clone,
+{
+    type Item = (Label, &'a BlockCC<Initiator, Instruction, Terminator>);
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut tail = true;
+        while let Some(label) = self.todo.last().map(|x| x.clone()) {
+            if let Some(block) = self.graph.labels.map.get(&label) {
+                for successor in block.terminator.successors() {
+                    if self.visited.insert(successor.clone()) {
+                        tail = false;
+                        self.todo.push(successor);
+                        break;
+                    }
+                }
+                if tail {
+                    self.todo.pop();
+                    return Some((label.clone(), block));
+                }
+            } else {
+                self.todo.pop();
+            }
+        }
+        None
+    }
+}
+
+impl<Label, Initiator, Instruction, Terminator> GraphOC<Label, Initiator, Instruction, Terminator>
+where
+    Terminator: Terminate<Label>,
+{
+    fn postorder<'a>(&'a self) -> Postorder<'a, Label, Initiator, Instruction, Terminator> {
+        let mut result = Postorder {
+            todo: Vec::new(),
+            visited: HashSet::new(),
+            graph: self,
+        };
+        result.todo.extend(self.entry.terminator.successors());
+        result
+    }
+}
