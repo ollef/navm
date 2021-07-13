0520215b0488de5ffab5966e71df2683aae89613
Olle Fredriksson <fredriksson.olle@gmail.com>
2021-07-14 00:36:51 +0200

    Move block add instances to separate module

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,9 @@
+mod add;
+pub use crate::block::add::*;
 use crate::graph::*;
+
 use std::collections::HashSet;
 use std::hash::Hash;
-use std::ops::Add;
 
 #[derive(Clone)]
 pub struct BlockOO<Instruction> {
@@ -73,63 +75,6 @@ impl<Initiator, Instruction> From<Initiator> for BlockCO<Initiator, Instruction>
             initiator: i,
             instructions: vec![],
         }
-    }
-}
-
-impl<Instruction> Add<Instruction> for BlockOO<Instruction> {
-    type Output = BlockOO<Instruction>;
-    fn add(mut self, instruction: Instruction) -> Self::Output {
-        self.instructions.push(instruction);
-        self
-    }
-}
-
-impl<Initiator, Instruction> Add<Instruction> for BlockCO<Initiator, Instruction> {
-    type Output = BlockCO<Initiator, Instruction>;
-    fn add(mut self, instruction: Instruction) -> Self::Output {
-        self.instructions.push(instruction);
-        self
-    }
-}
-
-impl<Instruction> Add<BlockOO<Instruction>> for BlockOO<Instruction> {
-    type Output = BlockOO<Instruction>;
-    fn add(mut self, mut rhs: BlockOO<Instruction>) -> Self::Output {
-        self.instructions.append(&mut rhs.instructions);
-        self
-    }
-}
-
-impl<Instruction, Terminator> Add<BlockOC<Instruction, Terminator>> for BlockOO<Instruction> {
-    type Output = BlockOC<Instruction, Terminator>;
-    fn add(mut self, mut rhs: BlockOC<Instruction, Terminator>) -> Self::Output {
-        self.instructions.append(&mut rhs.instructions);
-        BlockOC {
-            instructions: self.instructions,
-            terminator: rhs.terminator,
-        }
-    }
-}
-
-impl<Initiator, Instruction, Terminator> Add<BlockOC<Instruction, Terminator>>
-    for BlockCO<Initiator, Instruction>
-{
-    type Output = BlockCC<Initiator, Instruction, Terminator>;
-    fn add(mut self, mut rhs: BlockOC<Instruction, Terminator>) -> Self::Output {
-        self.instructions.append(&mut rhs.instructions);
-        BlockCC {
-            initiator: self.initiator,
-            instructions: self.instructions,
-            terminator: rhs.terminator,
-        }
-    }
-}
-
-impl<Initiator, Instruction> Add<BlockOO<Instruction>> for BlockCO<Initiator, Instruction> {
-    type Output = BlockCO<Initiator, Instruction>;
-    fn add(mut self, mut rhs: BlockOO<Instruction>) -> Self::Output {
-        self.instructions.append(&mut rhs.instructions);
-        self
     }
 }
 
---
+++ src/block/add.rs
@@ -0,0 +1,59 @@
+use crate::block::*;
+use std::ops::Add;
+
+impl<Instruction> Add<Instruction> for BlockOO<Instruction> {
+    type Output = BlockOO<Instruction>;
+    fn add(mut self, instruction: Instruction) -> Self::Output {
+        self.instructions.push(instruction);
+        self
+    }
+}
+
+impl<Initiator, Instruction> Add<Instruction> for BlockCO<Initiator, Instruction> {
+    type Output = BlockCO<Initiator, Instruction>;
+    fn add(mut self, instruction: Instruction) -> Self::Output {
+        self.instructions.push(instruction);
+        self
+    }
+}
+
+impl<Instruction> Add<BlockOO<Instruction>> for BlockOO<Instruction> {
+    type Output = BlockOO<Instruction>;
+    fn add(mut self, mut rhs: BlockOO<Instruction>) -> Self::Output {
+        self.instructions.append(&mut rhs.instructions);
+        self
+    }
+}
+
+impl<Instruction, Terminator> Add<BlockOC<Instruction, Terminator>> for BlockOO<Instruction> {
+    type Output = BlockOC<Instruction, Terminator>;
+    fn add(mut self, mut rhs: BlockOC<Instruction, Terminator>) -> Self::Output {
+        self.instructions.append(&mut rhs.instructions);
+        BlockOC {
+            instructions: self.instructions,
+            terminator: rhs.terminator,
+        }
+    }
+}
+
+impl<Initiator, Instruction, Terminator> Add<BlockOC<Instruction, Terminator>>
+    for BlockCO<Initiator, Instruction>
+{
+    type Output = BlockCC<Initiator, Instruction, Terminator>;
+    fn add(mut self, mut rhs: BlockOC<Instruction, Terminator>) -> Self::Output {
+        self.instructions.append(&mut rhs.instructions);
+        BlockCC {
+            initiator: self.initiator,
+            instructions: self.instructions,
+            terminator: rhs.terminator,
+        }
+    }
+}
+
+impl<Initiator, Instruction> Add<BlockOO<Instruction>> for BlockCO<Initiator, Instruction> {
+    type Output = BlockCO<Initiator, Instruction>;
+    fn add(mut self, mut rhs: BlockOO<Instruction>) -> Self::Output {
+        self.instructions.append(&mut rhs.instructions);
+        self
+    }
+}
